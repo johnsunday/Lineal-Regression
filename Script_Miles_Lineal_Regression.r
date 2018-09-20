74da82c9197b4c53958707045ff7157d8aa9aacb
Kevin Haunstetter <kevinhaunstetter@gmail.com>
2018-09-20 10:04:55 -0300

    Fix

--- Script_Miles_Lineal_Regression.r
+++ Script_Miles_Lineal_Regression.r
@@ -1,14 +1,15 @@
-#READ ANALYSIS FILE
-#FORMAT : 3 COLUMNS WITH HEADER: MDMVEHICLE_ID, MONTH, MILEAGE
 df<-read.csv(file.choose())
-
-# CONVERT VALUES TO NUMBERS
-df$"MONTH"<-as.numeric(df$"MONTH")
+colnames(df)[1] <- "SIMPLE_ID"
+colnames(df)[2] <- "UNIQUE_MONTH"
+colnames(df)[3] <- "MILEAGE"
+df$"UNIQUE_MONTH"<-as.numeric(df$"UNIQUE_MONTH")
 df$"MILEAGE"<-as.numeric(df$"MILEAGE")
 
-#CREATE RESULTS DATAFRAME
-results <- data.frame(MDMVEHICLE_ID=character(), 
-                 DATA_POINTS=numeric(), 
+starttime<-Sys.time()
+
+
+results <- data.frame(SIMPLE_ID=character(), 
+                 DATA_POINTS=character(), 
                  LINEAL_R_COEFF=double(),
                  LINEAL_R_CONST=double(),
                  CORR_COEFF=double(),
@@ -18,60 +19,64 @@ results <- data.frame(MDMVEHICLE_ID=character(),
                  LOWER_CONF_LIMIT_CONST=double(),
                  UPPER_CONF_LIMIT_CONST=double())
 
-#LOOP FOR EACH VEHICLE
-for (i in df$"VEHICLE_ID"){
+i<- subset(df,df$"SIMPLE_ID"<5000)
+i <- unique(i$"SIMPLE_ID")
+print ("Proccessing...")
+#loop for each vehicle
+for(id in i)
+  {
 
-#SUBSET FOR EACH VEHICLE    
-sub<- subset(df,df$"VEHICLE_ID"=i)
-#CORRELATION COEFFICIENT BETWEEN MILES AND MONTH FOR EACH VEHICLE
-CORR_COEFF<-cor(sub$"MONTH",sub$"MILEAGE")
-#LINEAL REGRESSION  FOR EACH VEHICLE
-model <- lm(sub$"MILEAGE" ~ sub$"MONTH")
+  
+#Subset for each Vehicle    
+sub<- subset(df,df$"SIMPLE_ID"==id)
+#Correlation Coefficient between Miles and Month for each Vehicle
+CORR_COEFF<-cor(sub$"UNIQUE_MONTH",sub$"MILEAGE")
+#Lineal Regression  for each vehicle
+model <- lm(sub$"MILEAGE" ~ sub$"UNIQUE_MONTH")
 
-#***************************************************
-# LINEAL REGRESSION FORMULA : 
-# Y= AX + B 
-#PREDICT_MILES = (UNIQUE_MONTH_TO_PREDICT * COEFFICIENT ) + CONSTANT
-#***************************************************
+#Insert new row with:
+# MDM_ID , nrow, Lineal Regression Coefficient ,Corrrelation Coefficient, R^2,
+#LEFT CONFIDENCE , RIGHT CONFIDENCE
 
-#GET LINEAL REGRESSION VALUES
-
-#GET COEFFICIENT AND CONSTANT VALUES
 regaux<-coef(model)
 
 LINEAL_R_CONST <-regaux["(Intercept)"]
-LINEAL_R_COEFF <-regaux["df$MONTH"]
+LINEAL_R_COEFF <-regaux["sub$UNIQUE_MONTH"]
 
-#GET CONFIDENCE INTERVAL
 confaux<-confint(model,level=0.95)
 
-LOWER_CONF_LIMIT_COEFF <-confaux["df$MONTH",1]
-UPPER_CONF_LIMIT_COEFF <-confaux["df$MONTH",2]
+LOWER_CONF_LIMIT_COEFF <-confaux["sub$UNIQUE_MONTH",1]
+UPPER_CONF_LIMIT_COEFF <-confaux["sub$UNIQUE_MONTH",2]
 LOWER_CONF_LIMIT_CONST <-confaux["(Intercept)",1]
 UPPER_CONF_LIMIT_CONST <-confaux["(Intercept)",2]
-
-#GET R SQUARED
 R_SQUARED<-summary(model)$r.squared
+SIMPLE_ID<-as.character(unique(sub$"SIMPLE_ID"))
+DATA_POINTS<-as.character(nrow(sub))
 
-#INSERT NEW ROW VALUES:
+row<- data.frame (SIMPLE_ID, 
+                  DATA_POINTS, 
+                  LINEAL_R_COEFF,
+                  LINEAL_R_CONST,
+                  CORR_COEFF,
+                  R_SQUARED,
+                  LOWER_CONF_LIMIT_COEFF,
+                  UPPER_CONF_LIMIT_COEFF,
+                  LOWER_CONF_LIMIT_CONST,
+                  UPPER_CONF_LIMIT_CONST)
 
-row<- list (MDMVEHICLE_ID=sub$"id", 
-            DATA_POINTS=nrow(sub), 
-            LINEAL_R_COEFF=LINEAL_R_COEFF,
-            LINEAL_R_CONST=LINEAL_R_CONST,
-            CORR_COEFF=CORR_COEFF,
-            R_SQUARED=R_SQUARED,
-            LOWER_CONF_LIMIT_COEFF=COEFF_LOWER_CONF,
-            UPPER_CONF_LIMIT_COEFF=COEFF_UPPER_CONF,
-            LOWER_CONF_LIMIT_CONST=CONST_LOWER_CONF,
-            UPPER_CONF_LIMIT_CONST=CONST_UPPER_CONF)
-              
+results <- bind_rows(results, row)
+}             
 
 #MERGE NEW ROW WITH RESULTS DATAFRAME
-results = rbind(results,row, stringsAsFactors=FALSE)
-}
-#CREATE CSV
+#results = rbind(results,row, stringsAsFactors=FALSE)
+
 archivename=paste("Lineal_Regression_Analysis_",Sys.Date(),".csv",sep="")
+print ("Writing CSV....")
 write.csv(results, archivename)
 #THE CSV WILL BE CREATED AT  C:\Users\Username\Documents
+
+endtime<-Sys.time()
+
 print ("THE PROCESS HAS ENDED!")
+print (paste("Start Time:",starttime))
+print (paste("End Time:",endtime))
